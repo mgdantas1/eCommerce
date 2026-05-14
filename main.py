4b067b5a24b2c7a53b28ad7ec059b2a1bc8ed631
Maria <garciamariadasgracas318@gmail.com>
2026-05-14 08:13:39 -0300

    feat: adicionar novos endpoints

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sqlmodel import select, Session
 from fastapi import FastAPI, Depends, HTTPException
 from database import create_db, get_session
 from contextlib import asynccontextmanager
-from models import Usuarios, Papeis
+from models import Usuarios, Papeis, Produtos, Categorias, Pedidos
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
@@ -13,6 +13,8 @@ async def lifespan(app:FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+# =-= USERS =-=
 
 @app.get('/users')
 def listar_usuarios(session: SessionDep) -> list[Usuarios]:
@@ -45,7 +47,7 @@ def editar_papeis(session: SessionDep, id: int, user: Usuarios) -> Usuarios:
     raise HTTPException(status_code=400, detail="Usuário não existe")
 
 @app.delete('/users')
-def excluir_usuarios(session: SessionDep, id: int):
+def excluir_usuarios(session: SessionDep, id: int) -> str:
     usuario = session.get(Usuarios, id)
     if usuario:
         session.delete(usuario)
@@ -53,6 +55,8 @@ def excluir_usuarios(session: SessionDep, id: int):
         return "Usuário deletado com sucesso"
 
     raise HTTPException(status_code=400, detail="Usuário não existe")
+
+# =-= PAPEIS =-=
 
 @app.get('/papeis')
 def listar_papeis(session: SessionDep) -> list[Papeis]:
@@ -79,7 +83,7 @@ def editar_papeis(session: SessionDep, id: int, papel: Papeis) -> Papeis:
     raise HTTPException(status_code=400, detail="O papel não existe")
 
 @app.delete('/papeis')
-def excluir_papeis(session: SessionDep, id: int):
+def excluir_papeis(session: SessionDep, id: int) -> str:
     papel = session.get(Papeis, id)
     if papel:
         session.delete(papel)
@@ -87,3 +91,112 @@ def excluir_papeis(session: SessionDep, id: int):
         return "Papel deletado com sucesso"
     
     raise HTTPException(status_code=400, detail="Papel não existe")
+
+# =-= PRODUTOS =-=
+
+@app.get('/produtos')
+def listar_produtos(session: SessionDep) -> list[Produtos]:
+    produtos = session.exec(select(Produtos)).all()
+    return produtos
+
+@app.post('/produtos')
+def adicionar_produtos(session: SessionDep, produto: Produtos) -> Produtos:
+    session.add(produto)
+    session.commit()
+    session.refresh(produto)
+    return produto
+
+@app.put('/produtos')
+def editar_produtos(session: SessionDep, produto: Produtos, id: int) -> Produtos:
+    produtoUpdate = session.get(Produtos, id)
+    if produtoUpdate:
+        produtoUpdate.nome = produto.nome
+        produtoUpdate.descricao = produto.descricao
+        produtoUpdate.preco = produto.preco
+        session.add(produtoUpdate)
+        session.commit()
+        session.refresh(produtoUpdate)
+        return produtoUpdate
+
+    raise HTTPException(status_code=400, detail="O produto não existe")
+
+@app.delete('/produtos')
+def excluir_produtos(session: SessionDep, id: int) -> str:
+    produto = session.get(Produtos, id)
+    if produto:
+        session.delete(produto)
+        session.commit()
+        return "Produto deletado com sucesso"
+    
+    raise HTTPException(status_code=400, detail='O produto não existe')
+
+# =-= CATEGORIAS =-=
+
+@app.get('/categorias')
+def listar_categoria(session: SessionDep) -> list[Categorias]:
+    categorias = session.exec(select(Categorias)).all()
+    return categorias
+
+@app.post('/categorias')
+def cadastrar_categoria(session: SessionDep, categoria: Categorias) -> Categorias:
+    session.add(categoria)
+    session.commit()
+    session.refresh(categoria)
+    return categoria
+
+@app.put('/categorias')
+def editar_categoria(session: SessionDep, categoria: Categorias, id: int) -> Categorias:
+    categoriaUpdate = session.get(Categorias, id)
+    if categoriaUpdate:
+        categoriaUpdate.nome = categoria.nome
+        session.add(categoriaUpdate)
+        session.commit()
+        session.refresh(categoriaUpdate)
+        return categoriaUpdate
+    
+    raise HTTPException(status_code=400, detail='Categoria não encontrada')
+
+@app.delete('/categorias')
+def excluir_categoria(session: SessionDep, id: int):
+    categoria = session.get(Categorias, id)
+    if categoria:
+        session.delete(categoria)
+        session.commit()
+        return 'Categoria deletada com sucesso'
+    
+    raise HTTPException(status_code=400, detail="Categoria não encontrada")
+
+# =-= PEDIDOS =-=
+
+@app.get('/pedidos')
+def listar_pedidos(session: SessionDep) -> list[Pedidos]:
+    pedidos = session.exec(select(Pedidos)).all()
+    return pedidos
+
+@app.post('/pedidos')
+def cadastrar_pedidos(session: SessionDep, pedido: Pedidos) -> Pedidos:
+    verificarID = session.get(Usuarios, pedido.usuario_id)
+    if verificarID:
+        session.add(pedido)
+        session.commit()
+        session.refresh(pedido)
+        return pedido
+    
+    raise HTTPException(status_code=400, detail='Usuário não existe')
+
+@app.put('/pedidos')
+def editar_pedidos(session: SessionDep, pedido: Pedidos, id: int) -> Pedidos:
+    pedidoUpdate = session.get(Pedidos, id)
+    if pedidoUpdate:
+        pedidoUpdate.total = pedido.total
+        pedidoUpdate.status = pedido.status
+        session.add(pedidoUpdate)
+        session.commit()
+        session.refresh(pedidoUpdate)
+        return pedidoUpdate
+    
+    raise HTTPException(status_code=400, detail='Pedido não encontrado')
+
+@app.delete('/pedidos')
+def excluir_pedidos(session: SessionDep):
+    pass
--- models.py
+++ models.py
@@ -7,7 +7,9 @@ class Usuarios(SQLModel, table=True):
     email: str = Field(max_length=150, unique=True)
     senha_hash: str = Field(max_length=255)
 
-    # pedidos:  
+    pedidos: list["Pedidos"] = Relationship(back_populates="usuario")
+    enderecos: list["Enderecos"] = Relationship(back_populates="usuario")
+    avaliacoes: list["Avaliacoes"] = Relationship(back_populates="usuario")
 
 class Papeis(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -17,7 +19,10 @@ class Produtos(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nome: str = Field(max_length=150)
     descricao: str
-    preco: Decimal = Field(max_digits=10, decimal_places=2)
+    preco: int
+
+    avaliacoes: list["Avaliacoes"] = Relationship(back_populates="produto")
+    estoque: list["Estoque"] = Relationship(back_populates="produto")
 
 class Categorias(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -26,16 +31,19 @@ class Categorias(SQLModel, table=True):
 class Pedidos(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     usuario_id: int = Field(foreign_key="usuarios.id")
-    total: Decimal = Field(max_digits=10, decimal_places=2)
+    total: int
     status: str = Field(max_length=50)
-    usuario: "Usuarios" = Relationship(back_populates='usuarios')
+    
+    usuario: "Usuarios" = Relationship(back_populates='pedidos')
+    pagamentos: list["Pagamentos"] = Relationship(back_populates="pedido")
 
 class Pagamentos(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     pedido_id: int = Field(foreign_key="pedidos.id")
     valor: Decimal = Field(max_digits=10, decimal_places=2)
     metodo: str = Field(max_length=50)
-    pedido: "Pedidos" = Relationship(back_populates="pedidos")
+
+    pedido: "Pedidos" = Relationship(back_populates="pagamentos")
 
 class Enderecos(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -44,7 +52,8 @@ class Enderecos(SQLModel, table=True):
     cidade: str = Field(max_length=100)
     estado: str = Field(max_length=100)
     cep: str = Field(max_length=20)
-    usuario: "Usuarios" = Relationship(back_populates='usuarios')
+
+    usuario: "Usuarios" = Relationship(back_populates='enderecos')
 
 class Avaliacoes(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -52,11 +61,13 @@ class Avaliacoes(SQLModel, table=True):
     produto_id: int = Field(foreign_key="produtos.id")
     nota: int
     comentario: str
-    usuario: "Usuarios" = Relationship(back_populates='usuarios')
-    produto: "Produtos" = Relationship(back_populates='produtos')
+
+    usuario: "Usuarios" = Relationship(back_populates='avaliacoes')
+    produto: "Produtos" = Relationship(back_populates='avaliacoes')
 
 class Estoque(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     produto_id: int = Field(foreign_key="produtos.id")
     quantidade: int
-    produto: "Produtos" = Relationship(back_populates='produtos')
+
+    produto: "Produtos" = Relationship(back_populates='estoque')
